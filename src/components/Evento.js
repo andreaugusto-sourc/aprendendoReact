365c5a07293b843d74007e3de5e4ba4202fc9136
andreaugusto-sourc <andreaugusto1033@gmail.com>
2023-02-13 19:14:23 -0300

    Uso da renderização condicional

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,13 +1,18 @@
-function Evento({numero}){
+import Button from './evento/Button';
 
+function Evento(){
     function meuEvento() {
-        alert(`Evento ativado! ${numero}`);
+        alert(`Ativando primeiro evento!`);
+    }
+    function segundoEvento() {
+        alert(`Ativando segundo evento!`);
     }
 
     return(
         <>
-        <p>Clique para disparar um evento</p>
-        <button onClick={meuEvento}>Disparar</button>
+            <p>Clique para disparar um evento</p>
+            <Button event={meuEvento} text="Primeiro evento" />
+            <Button event={segundoEvento} text="Segundo evento" />
         </>
     );
 }
