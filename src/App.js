4c5695a360cc70974fc3c5ea5a07b8fc377daad9
andreaugusto-sourc <andreaugusto1033@gmail.com>
2023-02-14 21:27:25 -0300

    Renderização de listas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import Condicional from './components/Condicional';
-
+import OutraLista from './components/OutraLista';
 
 function App() {
+
+  const minhaLista = ['React','Vue','Angular'];
+
   return (
     <div className="App">
-      <h1>Renderização Condicional</h1>
-      <Condicional />
+      <h1>Renderização de listas</h1>
+      <OutraLista itens={minhaLista} />
+      <OutraLista itens={[]} />
     </div>
   );
 }
